3c20e406b7dc693721d59a9854a4b854781fe24e
Greg Navis <contact@gregnavis.com>
2016-03-17 10:31:21 +0100

    Add tests for CampaignSender

--- hackout_test.py
+++ hackout_test.py
@@ -1,6 +1,46 @@
 import unittest
 
-from hackout import Email
+from hackout import CampaignEmail, CampaignSender, Email
+
+
+EMAIL1 = Email(from_='sender@example.com', to='recipient@example.com',
+               subject='Hello, World!', content='PS This is a test.')
+EMAIL2 = Email(from_='other.sender@example.com', to='recipient@example.com',
+               subject='Hey, There!', content='Another test.')
+EMAIL3 = Email(from_='sender@example.com', to='other.recipient@example.com',
+               subject='Hey, There!', content='Another test.')
+
+class CampaignSenderTest(unittest.TestCase):
+    CAMPAIGN_EMAIL1 = CampaignEmail(campaign='Test Campaign', email=EMAIL1)
+    CAMPAIGN_EMAIL2 = CampaignEmail(campaign='Test Campaign', email=EMAIL2)
+    CAMPAIGN_EMAIL3 = CampaignEmail(campaign='Test Campaign', email=EMAIL3)
+
+    ANOTHER_CAMPAIGN_EMAIL1 = CampaignEmail(campaign='Another Campaign',
+                                            email=EMAIL1)
+
+    def setUp(self):
+        self.client = FakeEmailClient();
+        self.sender = CampaignSender(self.client)
+
+    def test_send_one_email(self):
+        self.sender.send([self.CAMPAIGN_EMAIL1])
+
+        self.assertIn('Outreach', self.client.labels)
+        self.assertIn('Outreach/Test Campaign', self.client.labels)
+        self.assertEqual(1, len(self.client.emails))
+        self.assertEqual(['Outreach/Test Campaign'], self.client.labelling[0])
+
+    def test_send_at_most_once(self):
+        self.sender.send([self.CAMPAIGN_EMAIL1])
+        self.sender.send([self.CAMPAIGN_EMAIL2])
+
+        self.assertEqual(1, len(self.client.emails))
+
+    def test_send_many_emails(self):
+        self.sender.send([self.CAMPAIGN_EMAIL1, self.CAMPAIGN_EMAIL3,
+                          self.ANOTHER_CAMPAIGN_EMAIL1])
+
+        self.assertEqual(3, len(self.client.emails))
 
 
 class FakeEmailClient(object):
@@ -12,9 +52,7 @@ class FakeEmailClient(object):
     def create_label(self, name):
         if name not in self.labels:
             self.labels.append(name)
-            return len(self.labels) - 1
-        else:
-            return self.labels.index(name)
+        return name
 
     def has_sent(self, to, label_id):
         return any(
@@ -28,35 +66,33 @@ class FakeEmailClient(object):
         return len(self.emails) - 1
 
     def add_label(self, message_id, label_id):
-        if not (0 <= label_id < len(self.labels)):
+        if label_id not in self.labels:
             raise RuntimeError("unknown label ID %r" % label_id)
         self.labelling[message_id].append(label_id)
 
 
 class FakeEmailClientTest(unittest.TestCase):
-    EMAIL = Email(from_='sender@example.com', to='recipient@example.com',
-                  subject='Hello, World!', content='PS This is a test.')
-
     def setUp(self):
         self.client = FakeEmailClient()
 
     def test_labels_are_created_once(self):
-        self.assertEqual(0, self.client.create_label('Campaign'))
-        self.assertEqual(0, self.client.create_label('Campaign'))
+        self.assertEqual('Campaign', self.client.create_label('Campaign'))
+        self.assertEqual('Campaign', self.client.create_label('Campaign'))
+        self.assertEqual(1, len(self.client.labels))
 
     def test_emails_are_sent(self):
-        self.assertEqual(0, self.client.send_email(self.EMAIL))
-        self.assertEqual([self.EMAIL], self.client.emails)
+        self.assertEqual(0, self.client.send_email(EMAIL1))
+        self.assertEqual([EMAIL1], self.client.emails)
 
     def test_label_can_be_added(self):
         label_id = self.client.create_label('Campaign')
-        message_id = self.client.send_email(self.EMAIL)
+        message_id = self.client.send_email(EMAIL1)
         self.client.add_label(message_id, label_id)
 
-        self.assertTrue(self.client.has_sent(self.EMAIL.to, label_id))
+        self.assertTrue(self.client.has_sent(EMAIL1.to, label_id))
 
     def test_can_add_only_existent_label(self):
-        message_id = self.client.send_email(self.EMAIL)
+        message_id = self.client.send_email(EMAIL1)
 
         with self.assertRaises(RuntimeError):
             self.client.add_label(message_id, 0)
@@ -64,15 +100,15 @@ class FakeEmailClientTest(unittest.TestCase):
     def test_has_sent(self):
         label_id = self.client.create_label('Campaign')
 
-        self.assertFalse(self.client.has_sent(self.EMAIL.to, label_id))
+        self.assertFalse(self.client.has_sent(EMAIL1.to, label_id))
 
-        message_id = self.client.send_email(self.EMAIL)
+        message_id = self.client.send_email(EMAIL1)
 
-        self.assertFalse(self.client.has_sent(self.EMAIL.to, label_id))
+        self.assertFalse(self.client.has_sent(EMAIL1.to, label_id))
 
         self.client.add_label(message_id, label_id)
 
-        self.assertTrue(self.client.has_sent(self.EMAIL.to, label_id))
+        self.assertTrue(self.client.has_sent(EMAIL1.to, label_id))
 
 
 if __name__ == '__main__':
